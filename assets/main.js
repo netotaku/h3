ea879bfba0ef238b8669eedf5e0ea4c28882db6f
Glyn Morgan <glyn.morgan@wearehalo.co.uk>
2019-09-02 12:11:02 +0100

    Make draggable elements work with multiple per page

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', load, false);
 
 function load() {
 
-  const slider = document.querySelector('.slider');
+  const sliders = document.querySelectorAll('.slider');
   let isDown = false;
   let startX;
   let scrollLeft;
@@ -10,43 +10,45 @@ function load() {
   let startingPos = [];
   let childLinks;
 
-  slider.addEventListener('mousedown', function(e) {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-    isDragging = false;
-    startingPos = [e.pageX, e.pageY];
-  });
-  slider.addEventListener('mouseleave', function(e) {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mouseup', function(e) {
-    isDown = false;
-    slider.classList.remove('active');
-    for (var i = 0, element; element = childLinks[i]; i++) {
-      setTimeout(removeBlocking, 250, element);
-    }
-  });
-  slider.addEventListener('mousemove', function(e) {
-    if(!isDown) return;
-
-    if (!(e.pageX === startingPos[0] && e.pageY === startingPos[1])) {
-      isDragging = true;
-    }
-    if (isDragging) {
-      childLinks = document.querySelectorAll('.slider a');
+  for (var i = 0, slider; slider = sliders[i]; i++) {
+    slider.addEventListener('mousedown', function(e) {
+      isDown = true;
+      this.classList.add('active');
+      startX = e.pageX - this.offsetLeft;
+      scrollLeft = this.scrollLeft;
+      isDragging = false;
+      startingPos = [e.pageX, e.pageY];
+    });
+    slider.addEventListener('mouseleave', function(e) {
+      isDown = false;
+      this.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', function(e) {
+      isDown = false;
+      this.classList.remove('active');
       for (var i = 0, element; element = childLinks[i]; i++) {
-        element.addEventListener('click', addBlocking);
+        setTimeout(removeBlocking, 250, element);
       }
-    }
+    });
+    slider.addEventListener('mousemove', function(e) {
+      if(!isDown) return;
 
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 1.2;
-    slider.scrollLeft = scrollLeft - walk;
-  });
+      if (!(e.pageX === startingPos[0] && e.pageY === startingPos[1])) {
+        isDragging = true;
+      }
+      if (isDragging) {
+        childLinks = document.querySelectorAll('.slider a');
+        for (var i = 0, element; element = childLinks[i]; i++) {
+          element.addEventListener('click', addBlocking);
+        }
+      }
+
+      e.preventDefault();
+      const x = e.pageX - this.offsetLeft;
+      const walk = (x - startX) * 1.2;
+      this.scrollLeft = scrollLeft - walk;
+    });
+  }
 
   function addBlocking(e) {
     e.preventDefault();
